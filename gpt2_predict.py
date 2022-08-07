8a36e3380a5f9d6752ae1502f730ea2ee1814552
NilsB98 <nils.braehmer@gmx.de>
2022-08-07 17:26:13 +0200

    Added analysis and training results and generated tweets

--- gpt2_predict.py
+++ gpt2_predict.py
@@ -1,9 +1,30 @@
 from transformers import GPT2Tokenizer, pipeline, GPT2LMHeadModel, GPT2Config
+from pathlib import Path
+import pandas as pd
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+selection = "gpt2_net_scratch"
+input_prompts = ["Our goal", "Tesla", "This", "The", "After", "Next", "Today", "I", "Yes"]
+returned_seqs = 2
+
+options = {
+    "gpt2_fine_tuned": {
+        "tokenizer": GPT2Tokenizer.from_pretrained("gpt2"),
+        "dir_name": "fine_tuned"
+    },
+    "gpt2_net_scratch": {
+        "tokenizer": GPT2Tokenizer.from_pretrained("gpt2"),
+        "dir_name": "from_scratch"
+    },
+    "gpt2_tokenizer_scratch": {
+        "tokenizer": GPT2Tokenizer('gpt2tokenizer/vocab.json', 'gpt2tokenizer/merges.txt'),
+        "dir_name": "token_trained"
+    }
+}
+
+tokenizer = options[selection]["tokenizer"]
 tokenizer.pad_token = tokenizer.eos_token
-config = GPT2Config.from_json_file("./models/fine_tuned/config.json")
-model = GPT2LMHeadModel.from_pretrained("./models/fine_tuned")
+config = GPT2Config.from_json_file(f"./models/{options[selection]['dir_name']}/config.json")
+model = GPT2LMHeadModel.from_pretrained(f"./models/{options[selection]['dir_name']}")
 
 generate = pipeline(
     "text-generation",
@@ -12,5 +33,22 @@ generate = pipeline(
     device=0
 )
 
-tx = generate("Tesla is", max_length=30, num_return_sequences=5)
-print(tx)
+
+generation_outputs = generate(input_prompts, max_length=30, num_return_sequences=returned_seqs)
+
+file_path = f"./sample_generated_tweets/{selection}.csv"
+file = Path(file_path)
+file.parent.mkdir(exist_ok=True, parents=True)
+
+outputs = []
+prompts = []
+for output, prompt in zip(generation_outputs, input_prompts):
+    outputs.extend(output)
+    prompts.extend([prompt] * returned_seqs)
+
+data = [[prompt, output['generated_text']] for prompt, output in zip(prompts, outputs)]
+df = pd.DataFrame(data=data, columns=['prompt', 'output'])
+
+df.to_csv(file_path, mode="w+")
+
+print(generation_outputs)
